fe7752617f286ba04756deff69e8d6525c467b0f
mxklb <mxklb@protonmail.com>
2020-06-05 16:28:07 +0200

    Change source url in setup py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name = "pylightdmx",
 	license = "GPLV3",
 	author = "ayork02",
 	author_email = "ayork02@outlook.com",
-	url = "https://github.com/ayork02/pylightdmx",
+	url = "https://github.com/mxklb/pylightdmx",
 	classifiers = [
 		"Development Status :: 3 - Alpha",
 		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
